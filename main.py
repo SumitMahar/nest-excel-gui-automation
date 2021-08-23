a49bdf1b936dbe29e9eaeeea3364e038029dd9d4
Sumit Mahar <SumitMahar78@gmail.com>
2021-08-23 22:36:13 +0530

    created a function to go to a specific cell

--- main.py
+++ main.py
@@ -7,5 +7,9 @@ def ref_market_watch():
 	pg.rightClick(147, 230, duration=.3)
 	pg.leftClick(209, 645, duration=.4)
 
-
-
+def change_name_box_to(cell):
+    pg.doubleClick(764, 153, duration=.1)
+    pg.press('enter')
+    pg.click(764, 153, duration=.1)
+    pg.write(cell)
+    pg.press('enter')
