ddfc619632cdc6b8ee8d76dd480734a6a3ae2a9f
Sumit Mahar <SumitMahar78@gmail.com>
2021-08-29 15:53:00 +0530

    coded alternative way

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ pg.FAILSAFE = True
 
 # this function is for refreshing the market watch
 def ref_market_watch(rx=147, ry=230, lx=209, ly=645):
-	pg.rightClick(rx, ry, duration=.3)
-	pg.leftClick(lx, ly, duration=.4)
+	pg.rightClick(rx, ry, duration=.1)
+	pg.leftClick(lx, ly, duration=.1)
 
 right_arrow_count = 3
 # this is for excel gui automation
@@ -21,37 +21,66 @@ def change_name_box_to(cell, x=764, y=153):
     pg.press('enter')
 
 def copy_and_paste_columns():
-    # copies the two specific columns 
-    pg.keyDown('shift')
-    time.sleep(.1)
-    pg.keyDown('ctrl')
-    time.sleep(.1)
+    # copies the two specific columns
+    '''
+    "does not work in some systems
+    an alternative code is written below"
+    
+    pg.keyDown('shiftleft')
+    time.sleep(.2)
+    pg.keyDown('ctrlleft')
+    time.sleep(.2)
     pg.press(['right','down'])
     pg.keyUp('shift')
+    time.sleep(.1)
     pg.press('c')
     pg.keyUp('ctrl')
+    '''
+
+    # Alternative to above code
+    # we will have to specify the x and y values
+    # of the first column
+    # to know x and y values use the pyautogui.displayMousePosition() function
+    x = 1000
+    y = 185
+    pg.click(x, y)
+    pg.dragTo(x+60, y)
+    pg.hotkey('ctrl', 'c')
+
+    # this might be even more simpler if you define a name for
+    # the columns that you want to copy from excel sheet, 
+    # for defining name select the columns right click and then click on define name
+    # then instead of writing A1 in the name box we will simply write defined name for
+    # the columns to copy and they will be auto selected and the we will only have to copy and paste the values
 
     global right_arrow_count
     pg.press('right', presses=right_arrow_count)
     right_arrow_count += 3
 
     # this is for pasting the values only
-    pg.hotkey('alt', 'e', 's', 'v')
-    pg.press('enter')
+    pg.hotkey('ctrl', 'v')
+    time.sleep(.1)
+    pg.press(['ctrl', 'v'])
+    pg.press('escape')
 
 # main loop
 interval = 15
 
 print('Starting the programe...\nPlease minimize this window.')
-time.sleep(8) # 8 seconds of pause for minimizing the window.
-while interval < 120:
+time.sleep(5) # 8 seconds of pause for minimizing the window.
+while interval < 240:
     try:
         print(f'Refreshing the Market-Watch : {time.asctime()}', end='\n')
         ref_market_watch()
-        change_name_box_to('f1')
+        change_name_box_to('A1')
         copy_and_paste_columns()
         interval += 15
         time.sleep(5) # 900 sec : 15 minutes
-    except:
+    except Exception as e:
         print('Error..! Please setup the Excel and Nest windows accordingly.')
-        time.sleep(20)
+        print('Exception was:', e)
+        pg.keyUp('shift')
+        pg.keyUp('ctrl')
+        time.sleep(2)
+        print('R.A.Count: ', right_arrow_count)
+        break
